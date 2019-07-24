bfd6575d072179d132a6d171016cf209a100ff60
Michael McGuire <michael3mcguire@gmail.com>
2019-07-24 14:50:16 -0400

    Updates

---
+++ precache-manifest.b632c5b0f3244a4bc86b8aa7ce17f86e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f81bed6d20a8e0a770cfd6035495c0e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f3844bb374a1d85c9dcb",
+    "url": "/Portfolio/static/css/main.53a3b947.chunk.css"
+  },
+  {
+    "revision": "0d6ac7dac0dd170142b7",
+    "url": "/Portfolio/static/js/2.918ef874.chunk.js"
+  },
+  {
+    "revision": "f3844bb374a1d85c9dcb",
+    "url": "/Portfolio/static/js/main.97417743.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  }
+]);
